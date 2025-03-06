78556a6b06942bfe11d7725e165197d8d3320458
Jhon Munarco Santos <105329243+jhonsantos94@users.noreply.github.com>
2025-03-06 10:10:31 -0500

    Add files via upload

--- Ecommerce/build-backend.sh
+++ Ecommerce/build-backend.sh
@@ -1,41 +1,44 @@
 #!/bin/bash
 
-# Guarda el directorio actual
-rootpath=$(pwd)
+# Obtener la ruta del script
+rootpath="$(dirname "$(realpath "$0")")"
 
-# Navega a la carpeta 'backend'
 cd "$rootpath/backend"
 
-# Establece las rutas de los servicios
-auth_service_path="$PWD/msvc-auth"
-gateway_service_path="$PWD/msvc-gateway"
-eureka_service_path="$PWD/msvc-registry"
-users_service_path="$PWD/msvc-users"
-courses_service_path="$PWD/msvc-courses"
+ecovida_inventory_service_path="$PWD/ecovida-inventory-service"
+ecovida_order_service_path="$PWD/ecovida-order-service"
+ecovida_product_service_path="$PWD/ecovida-product-service"
+ecovida_user_service_path="$PWD/ecovida-user-service"
+eureka_server_path="$PWD/eureka-server"
+gateway_service_path="$PWD/gateway-service"
+msvc_auth_path="$PWD/msvc-auth"
+
+chmod +x "$ecovida_inventory_service_path/build.sh"
+chmod +x "$ecovida_order_service_path/build.sh"
+chmod +x "$ecovida_product_service_path/build.sh"
+chmod +x "$ecovida_user_service_path/build.sh"
+chmod +x "$eureka_server_path/build.sh"
+chmod +x "$gateway_service_path/build.sh"
+chmod +x "$msvc_auth_path/build.sh"
 
 echo "Building backend services"
 
-echo "Building auth service"
-cd "$auth_service_path"
-./build.sh
+services=(
+    "inventory service:$ecovida_inventory_service_path"
+    "order service:$ecovida_order_service_path"
+    "product service:$ecovida_product_service_path"
+    "user service:$ecovida_user_service_path"
+    "eureka server:$eureka_server_path"
+    "gateway service:$gateway_service_path"
+    "auth service:$msvc_auth_path"
+)
 
-echo "Building gateway service"
-cd "$gateway_service_path"
-./build.sh
-
-echo "Building eureka service"
-cd "$eureka_service_path"
-./build.sh
-
-echo "Building users service"
-cd "$users_service_path"
-./build.sh
-
-echo "Building courses service"
-cd "$courses_service_path"
-./build.sh
+for service in "${services[@]}"; do
+    IFS=":" read -r name path <<< "$service"
+    echo "Building $name"
+    cd "$path" || exit 1
+    ./build.sh || { echo "Error building $name"; exit 1; }
+done
 
 echo "Backend services build completed"
-
-# Regresa al directorio original
 cd "$rootpath"
